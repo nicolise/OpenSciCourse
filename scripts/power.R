afc3bb1834e4d986451e04130e902955cb74de89
Nicole E Soltis <ne.soltis@gmail.com>
2016-05-04 11:54:31 -0700

    minor comments during workshop

--- scripts/power.R
+++ scripts/power.R
@@ -6,7 +6,11 @@ data %>%
   group_by(gend_gendobs) %>%
   tally()
 
+#want 80% power, what effect size can I detect? If effect size > 0.6, study has sufficient power.
 detectable <- pwr.t2n.test(n1 = 24, n2 = 54, d = NULL, sig.level = .05, power = .8, alternative = "two.sided")
+
+#adding covariates can account for noise, increase power
 
 pwr.t2n.test(n1 = 24, n2 = 54, d = .5, sig.level = .05, power = NULL, alternative = "two.sided")
 
+#use power analysis to make decisions about statistical design, study design
